266b406083ea0492bcd7be2ee0b1516154fb6db5
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-02-28 13:02:59 +0100

    Create Navigation and Routing

---
+++ components/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/technologies">Technologies</NuxtLink>
+    <NuxtLink to="/projects">Projects</NuxtLink>
+    <NuxtLink to="/contact">Contact</NuxtLink>
+  </div>
+</template>
