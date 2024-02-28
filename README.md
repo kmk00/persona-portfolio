d06edaf27def1db7484bddbf551b412b2c81c543
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-02-28 12:47:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# persona-portfolio-website
+ 
---
+++ nuxt.config.ts
@@ -0,0 +1,43 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ["@/assets/styles/main.css"],
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            customRed: {
+              100: "#F50000",
+              200: "#C10000",
+              300: "#B90000",
+              400: "#6A0202",
+            },
+            customGray: {
+              100: "#D3D3D3",
+              200: "#686868",
+              300: "#404040",
+            },
+            customYellow: "#D5C31C",
+            customPing: {
+              100: "#FD86CB",
+              200: "#EF637B",
+            },
+            customBlue: {
+              100: "#00C5D0",
+              200: "#008D95",
+              300: "#0056FF",
+            },
+          },
+          fontFamily: {
+            expose: ["expose"],
+            markin: ["markin"],
+            p5shatty: ["p5shatty"],
+            prototype: ["prototype"],
+          },
+        },
+      },
+    },
+  },
+});
