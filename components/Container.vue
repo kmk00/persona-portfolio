0fcf7c6d4f1301cd3479bb033bce1d1f591d605b
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-02-29 14:48:12 +0100

    Create Home mobile

--- components/Container.vue
+++ components/Container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex md:justify-end md:mt-10 mt-44">
+  <div class="lg:mt-10 mt-44">
     <slot />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup>
+const navigation = useNavigation();
+</script>
 
 <template>
-  <div class="">
-    <h1>Hdsadsa</h1>
+  <div>
+    <div
+      class="text-customGray-100 font-markin p-3 text-5xl text-center relative flex flex-col justify-center items-center gap-8 mx-auto"
+    >
+      <p>Hi, I'm Lucas, Web Developer</p>
+      <p>Welcome to my portfolio</p>
+    </div>
+    <img
+      :class="navigation.isOpen ? 'hidden' : 'block'"
+      class="lg:hidden fixed bottom-10 opacity-20 scale-[.4] left-[50%] translate-x-[-50%]"
+      src="~/assets/shapes/Star.svg"
+    />
+    <img
+      :class="navigation.isOpen ? 'hidden' : 'block'"
+      class="lg:hidden fixed bottom-0 z-10"
+      src="~/assets/shapes/home-rect.svg"
+    />
+    <img
+      :class="navigation.isOpen ? 'hidden' : 'block'"
+      class="lg:hidden fixed bottom-0 right-0 z-0"
+      src="~/assets/shapes/home-rect2.svg"
+    />
   </div>
 </template>
