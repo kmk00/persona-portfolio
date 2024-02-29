2bb73a2333ff0499e1ddc79f90cd6a768a8952b5
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-02-29 18:10:43 +0100

    Create Stack Page

---
+++ components/StackItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { stackName } = defineProps<{
+  stackName: string;
+}>();
+</script>
+
+<template>
+  <div class="relative mx-auto w-[150px] lg:w-[200px] my-8">
+    <img
+      class="absolute bottom-0 right-[50%] translate-x-[50%]"
+      src="~/assets/shapes/stack-item.svg"
+    />
+    <p
+      class="text-xl lg:text-2xl absolute bottom-2 right-[50%] translate-x-[50%]"
+    >
+      {{ stackName }}
+    </p>
+  </div>
+</template>
--- pages/stack/index.vue
+++ pages/stack/index.vue
@@ -1,3 +1,40 @@
 <template>
-  <div>t</div>
+  <div>
+    <div class="font-expose px-4 my-8 text-xl">
+      <p class="text-4xl my-8 text-customGray-100 text-center">
+        Technologies and tools I've already used.
+      </p>
+      <div
+        class="grid pt-8 h-[300px] overflow-y-scroll overflow-x-hidden text-white grid-cols-2 sm:grid-cols-3 justify-center items-center text-center"
+      >
+        <StackItem stack-name="HTML" />
+        <StackItem stack-name="CSS" />
+        <StackItem stack-name="JAVASCRIPT" />
+        <StackItem stack-name="TYPESCRIPT" />
+        <StackItem stack-name="HTML" />
+        <StackItem stack-name="CSS" />
+        <StackItem stack-name="JAVASCRIPT" />
+        <StackItem stack-name="TYPESCRIPT" />
+        <StackItem stack-name="HTML" />
+        <StackItem stack-name="CSS" />
+        <StackItem stack-name="JAVASCRIPT" />
+        <StackItem stack-name="TYPESCRIPT" />
+        <StackItem stack-name="JAVASCRIPT" />
+        <StackItem stack-name="TYPESCRIPT" />
+        <StackItem stack-name="JAVASCRIPT" />
+        <StackItem stack-name="TYPESCRIPT" />
+      </div>
+      <p class="text-4xl my-8 text-customGray-100 text-right">
+        Things I want to learn next...
+      </p>
+      <div
+        class="grid h-[300px] gap-4 sm:mr-4 mx-auto sm:w-1/2 text-white grid-cols-2 justify-center items-center text-center"
+      >
+        <StackItem stack-name="HTML" />
+        <StackItem stack-name="CSS" />
+        <StackItem stack-name="JAVASCRIPT" />
+        <StackItem stack-name="TYPESCRIPT" />
+      </div>
+    </div>
+  </div>
 </template>
