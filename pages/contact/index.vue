88d0ffb245f72162b9bd9e49815daf86f0399c5d
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-03-01 13:58:44 +0100

    Add contact

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>C</div>
+  <div>
+    <div
+      class="font-expose p-8 lg:px-2 text-center text-3xl md:text-5xl lg:text-6xl xl:text-8xl text-white"
+    >
+      <p>Don’t hesitate to contact me...</p>
+      <div
+        class="flex flex-col gap-4 lg:gap-16 mt-8 lg:mt-24 text-6xl lg:text-8xl"
+      >
+        <a class="rotate-[-10deg]" href="https://github.com/kmk00">Github</a>
+        <a class="rotate-[4deg]" href="mailto:jacob.rockily@gmail.com">Mail</a>
+        <a class="rotate-[-6deg]" href="/resume.pdf" download="resume"
+          >Resume</a
+        >
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped></style>
