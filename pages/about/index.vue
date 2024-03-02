6a4f1ba97ac811f80a423a68486ef02acf556533
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-03-02 12:49:52 +0100

    Update index.vue

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -6,17 +6,44 @@ const navigation = useNavigation();
   <div>
     <div class="text-customGray-100 relative z-30 font-markin p-8 text-xl">
       <!-- :class="navigation.isOpen ? 'hidden' : 'block'" -->
-      <div class="transform -skew-y-6 relative z-0 bg-black p-2">
+      <div class="transform -skew-y-2 relative z-0 bg-black p-2">
         <div class="bg-white p-2">
           <div
-            class="bg-customGray-400 max-h-[300px] md:max-h-[400px] overflow-y-auto"
+            class="bg-customGray-400 max-h-[300px] md:max-h-[600px] xl:max-h-[800px] overflow-y-auto"
           >
-            <p class="p-4">
+            <p class="p-4 md:text-3xl lg:text-5xl">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make. Lorem Ipsum is simply dummy text of
               the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make.Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make.Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make.Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make.Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make.Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make.Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an the
+              printing and typesetting industry. Lorem Ipsum has been the
               industry's standard dummy text ever since the 1500s, when an
               unknown printer took a galley of type and scrambled it to make.
               Lorem Ipsum has been the industry's standard dummy text ever since
@@ -32,12 +59,12 @@ const navigation = useNavigation();
     </div>
     <img
       :class="navigation.isOpen ? 'hidden' : 'block'"
-      class="lg:hidden fixed bottom-0 z-0"
+      class="fixed lg:hidden bottom-0 z-0"
       src="~/assets/shapes/home-rect.svg"
     />
     <img
       :class="navigation.isOpen ? 'hidden' : 'block'"
-      class="lg:hidden fixed bottom-0 right-0 z-0"
+      class="fixed bottom-0 right-0 z-0"
       src="~/assets/shapes/home-rect2.svg"
     />
   </div>
