44fb8d06cd3ea4d795ccbe481b5908ebd859cd98
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-03-03 20:54:31 +0100

    Add Start page animation

--- components/StartPage.vue
+++ components/StartPage.vue
@@ -3,24 +3,39 @@ const startPage = useStartPage();
 </script>
 
 <template>
-  <div
-    v-if="startPage.isOpen"
-    class="fixed w-screen h-screen bg-black justify-center text-white items-center md:justify-end md:items-end z-50 flex"
-  >
-    <div class="group md:mr-20 md:mb-20 relative">
-      <img
-        class="hidden group-hover:block absolute"
-        src="~/assets/shapes/start-rect2.svg"
-      />
-      <button
-        @click="startPage.toggle"
-        class="font-prototype rotate-[-15deg] p-5 m-5 text-6xl"
+  <Transition name="fade">
+    <div class="fixed w-screen h-screen z-50" v-if="startPage.isOpen">
+      <div
+        class="justify-center w-screen h-screen bg-black text-white items-center md:justify-end md:items-end flex"
       >
-        S<span class="group-hover:text-black">T</span>A<span
-          class="group-hover:text-black"
-          >R</span
-        >T
-      </button>
+        <div class="group md:mr-20 md:mb-20 relative">
+          <img
+            class="opacity-0 group-hover:opacity-100 absolute transition-all ease-in-out duration-100"
+            src="~/assets/shapes/start-rect2.svg"
+          />
+          <button
+            @click="startPage.toggle"
+            class="font-prototype rotate-[-15deg] p-5 m-5 text-6xl"
+          >
+            S<span class="group-hover:text-black">T</span>A<span
+              class="group-hover:text-black"
+              >R</span
+            >T
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
